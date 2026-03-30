2bd4dca3b9ed858cfe80fc6765c63a287197c906
Sindhuja-R-23 <sk9028@srmist.edu>
2026-03-30 22:38:43 +0530

    UC7: Sort Bogies by Capacity

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,14 +1,25 @@
 import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public String toString() {
+        return name + "(" + capacity + ")";
+    }
+}
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        Map<String, Integer> bogieCapacity = new HashMap<>();
-        bogieCapacity.put("Sleeper", 72);
-        bogieCapacity.put("AC Chair", 56);
-        bogieCapacity.put("First Class", 24);
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
 
-        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue() + " seats");
-        }
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+        System.out.println("Sorted Bogies: " + bogies);
     }
 }
