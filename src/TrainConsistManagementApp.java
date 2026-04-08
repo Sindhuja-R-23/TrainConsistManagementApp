5631bf214d987ba901c5cb20edaf52c263d34241
Sindhuja-R-23 <sk9028@srmist.edu.in>
2026-04-08 14:20:32 +0530

    UC11: Validate Train ID & Cargo Codes (Regex)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,49 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("UC11 - Validate Train ID & Cargo Code");
+        System.out.println("====================================\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        // INPUT
+        System.out.print("Enter Train ID: ");
+        String trainId = sc.nextLine();
+
+        System.out.print("Enter Cargo Code: ");
+        String cargoCode = sc.nextLine();
+
+        // REGEX PATTERNS
+        String trainRegex = "TRN-\\d{4}";
+        String cargoRegex = "PET-[A-Z]{2}";
+
+        // COMPILE
+        Pattern trainPattern = Pattern.compile(trainRegex);
+        Pattern cargoPattern = Pattern.compile(cargoRegex);
+
+        // MATCH
+        Matcher trainMatcher = trainPattern.matcher(trainId);
+        Matcher cargoMatcher = cargoPattern.matcher(cargoCode);
+
+        // VALIDATION RESULT
+        if (trainMatcher.matches()) {
+            System.out.println("\nTrain ID is VALID");
+        } else {
+            System.out.println("\nTrain ID is INVALID");
+        }
+
+        if (cargoMatcher.matches()) {
+            System.out.println("Cargo Code is VALID");
+        } else {
+            System.out.println("Cargo Code is INVALID");
+        }
+
+        System.out.println("\nUC11 validation completed...");
+    }
 }
