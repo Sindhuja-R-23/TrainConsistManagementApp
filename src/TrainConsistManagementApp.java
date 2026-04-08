51733e8f8b1564c531fb157bad32ab3f141b74d4
Sindhuja-R-23 <sk9028@srmist.edu.in>
2026-04-08 15:00:35 +0530

    UC8

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,18 +1,41 @@
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String toString() {
+        return name + "(" + capacity + ")";
+    }
+}
 
 public class TrainConsistManagementApp {
+
     public static void main(String[] args) {
 
-        List<Bogie> bogies = Arrays.asList(
-                new Bogie("Sleeper", 72),
-                new Bogie("AC Chair", 56),
-                new Bogie("First Class", 24)
-        );
+        System.out.println("====================================");
+        System.out.println("UC8 - Filter Passenger Bogies");
+        System.out.println("====================================\n");
 
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 50));
+        bogies.add(new Bogie("Second Sitting", 80));
+
+        // FILTER: capacity > 70
         List<Bogie> filtered = bogies.stream()
-                .filter(b -> b.capacity > 60)
+                .filter(b -> b.capacity > 70)
                 .collect(Collectors.toList());
 
-        System.out.println("Filtered Bogies: " + filtered);}
+        System.out.println("Filtered Bogies:");
+        System.out.println(filtered);
+    }
 }
